c172d56e6050b7e144cc34d365b8a3036676632f
mohamtur1 <162804426+mohamtur1@users.noreply.github.com>
2026-05-14 09:36:30 +0000

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../4cbon-dev.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
